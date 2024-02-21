9382d11b26eed42a196f35e1df9bdc4197d9f404
ilzammuhtaromi3003 <ilzammuhtaromi3003@gmail.com>
2024-02-21 14:00:43 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Discover from "./pages/Discover";
+import Upcoming from "./pages/Upcoming";
+import TopRated from "./pages/TopRated";
+import Navbar from "./components/Navbar";
+
+const BASE_API_URL = "https://api.themoviedb.org/3";
+const API_KEY = "4c9648599d1ab76925b2526a4cb21186";
+
+function App() {
+  useEffect(() => {
+    getSession();
+  }, []);
+
+  const getSession = async () => {
+    const {
+      data: { guest_session_id },
+    } = await axios.get(`${BASE_API_URL}/authentication/guest_session/new`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    sessionStorage.setItem("session_id", `${guest_session_id}`);
+    console.log(guest_session_id);
+  };
+
+  return (
+    <Router>
+      <div className="App-header">
+        <nav className="NavBar">
+          <Navbar />
+        </nav>
+        <Routes>
+          <Route path="/" exact element={<Discover />} />
+          <Route path="/upcoming" exact element={<Upcoming />} />
+          <Route path="/top-rated" exact element={<TopRated />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+      <nav>
+        <h1 className="header-title">Movie App</h1>
+        <ul>
+          <li className="li">
+            <Link to="/">Discover</Link>
+          </li>
+          <li className="li">
+            <Link to="/upcoming">Upcoming</Link>
+          </li>
+          <li className="li">
+            <Link to="/top-rated">Top Rated</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
